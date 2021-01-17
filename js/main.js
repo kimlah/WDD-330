f42a7517acccd6466fa2bf890595b513c56b21b5
kimlah <71352287+kimlah@users.noreply.github.com>
2021-01-16 21:55:15 -0800

    update index, trying to get TOC working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-const links = [
-    {
-        label: "Week 1",
-        url: "../Week1/notes.html"
-    }
-]
+const file = './data/main.json'
+
+fetch(file)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (jsonObject) {
+        const contentList = jsonObject["links"];
+
+        for (let i = 0; i < links.length; i++) {
+            let toc = document.createElement("li");
+
+            let p = document.createElement("p");
+            p.textContent = links[i].label + links[i].url;
+            toc.appendChild(p);
+            document.querySelector("ol").appendChild(toc);
+        }
+    })
